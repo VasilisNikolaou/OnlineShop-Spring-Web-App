8526a39195a7c16f1cfe41603cfca0d9d69a2d09
Vasilis Nikolaou <basilisnik16@gmail.com>
2020-04-13 19:56:46 +0300

    Added images, css, and html files

--- OnlineShop/src/main/java/onlineshop/web/CartController.java
+++ OnlineShop/src/main/java/onlineshop/web/CartController.java
@@ -1,12 +1,16 @@
 package onlineshop.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
 @RequestMapping("/cart")
+@SessionAttributes("cart")
 public class CartController {
 
+	@GetMapping
 	public String showCart() {
 		return "cart";
 	}
