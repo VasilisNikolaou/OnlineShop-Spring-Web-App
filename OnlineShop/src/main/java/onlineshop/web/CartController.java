20eb824b22ab07ca65f1564e1798c02a45d049b8
Vasilis Nikolaou <basilisnik16@gmail.com>
2020-04-15 22:07:17 +0300

    Updated shopping cart functionality

--- OnlineShop/src/main/java/onlineshop/web/CartController.java
+++ OnlineShop/src/main/java/onlineshop/web/CartController.java
@@ -1,11 +1,18 @@
 package onlineshop.web;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import onlineshop.model.Cart;
+import onlineshop.model.Product;
 import onlineshop.service.ProductService;
 
 
@@ -24,5 +31,22 @@ public class CartController {
 	@GetMapping
 	public String showCart() {
 		return "cart";
+	}
+	
+	@PostMapping("/remove/{name}")
+	public String removeCartItem(@PathVariable("name") String name, @ModelAttribute("cart") Cart cart) {
+		  
+		  Optional<Product> product = productService.findByName(name);
+		
+		  if(product.isPresent()) {
+			  
+			   cart.getItems()
+			                  .removeIf(item -> item.getProduct().getId() == product.get().getId());
+			   
+			   cart.calculateTotalPrice();
+			  
+		  }
+		  
+		  return "redirect:/cart";
 	}
 }
