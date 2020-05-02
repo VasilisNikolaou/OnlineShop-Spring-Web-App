ac5c89ab5bf089d71bff1292758ed04a9e48fd91
Vasilis Nikolaou <basilisnik16@gmail.com>
2020-05-02 17:56:56 +0300

    Updated Security Configuration

--- OnlineShop/src/main/java/security/SecurityConfig.java
+++ OnlineShop/src/main/java/security/SecurityConfig.java
@@ -1,9 +1,61 @@
 package security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import onlineshop.service.UserPrincipalDetailsService;
 
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
+	private UserPrincipalDetailsService userPrincipalDetailsService;
 	
+	@Autowired
+	public SecurityConfig(UserPrincipalDetailsService userPrincipalDetailsService) {
+		this.userPrincipalDetailsService = userPrincipalDetailsService;
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http 
+		    .authorizeRequests()
+		      .antMatchers("/order/current").hasRole("USER")
+		      .antMatchers("/", "/**").permitAll()
+		    .and()
+		      .formLogin()
+		      .loginPage("/login").permitAll()
+		    .and()
+		      .rememberMe().key("uniqueAndSecret").userDetailsService(userPrincipalDetailsService)
+		    .and()
+		      .logout();
+		      
+		     
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		 auth
+		      .authenticationProvider(daoAuthenticationProvider());
+	}
+	
+	@Bean
+	public DaoAuthenticationProvider daoAuthenticationProvider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		
+		provider.setPasswordEncoder(passwordEncoder());
+		provider.setUserDetailsService(userPrincipalDetailsService);
+		
+		return provider;
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 }
