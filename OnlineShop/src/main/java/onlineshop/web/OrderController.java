660943091810c6c403affe77e19c8fa38dc7e8b6
Vasilis Nikolaou <basilisnik16@gmail.com>
2020-05-15 20:29:02 +0300

    updated order controller functionality

--- OnlineShop/src/main/java/onlineshop/web/OrderController.java
+++ OnlineShop/src/main/java/onlineshop/web/OrderController.java
@@ -1,19 +1,29 @@
 package onlineshop.web;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import onlineshop.model.UserPrincipal;
 import onlineshop.model.Order;
 
 @Controller
 @RequestMapping("/order/current")
 public class OrderController {
-
+	
 	@ModelAttribute("order")
 	public Order order() {
-		return new Order();
+		
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		UserPrincipal userPrincipal = (UserPrincipal) auth.getPrincipal();
+		
+		Order order = new Order();
+		order.setUser(userPrincipal.getUser());
+		
+		return order;
 	}
 	
 	@GetMapping
