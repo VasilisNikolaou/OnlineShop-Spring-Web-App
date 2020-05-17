d76c983420c390490ca71e95f65d0776839e253b
Vasilis Nikolaou <basilisnik16@gmail.com>
2020-05-17 20:30:17 +0300

    Finished order controller

--- OnlineShop/src/main/java/onlineshop/web/OrderController.java
+++ OnlineShop/src/main/java/onlineshop/web/OrderController.java
@@ -1,18 +1,43 @@
 package onlineshop.web;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import onlineshop.model.UserPrincipal;
+import onlineshop.model.Cart;
+import onlineshop.model.OrderItem;
+import onlineshop.model.User;
+import onlineshop.data.CartRepository;
+import onlineshop.data.UserRepository;
 import onlineshop.model.Order;
 
 @Controller
 @RequestMapping("/order/current")
+@SessionAttributes("cart")
 public class OrderController {
+	
+	private CartRepository cartRepo;
+	private UserRepository userRepo;
+	
+	@Autowired
+	public OrderController(CartRepository cartRepo, UserRepository userRepo) {
+		this.cartRepo = cartRepo;
+		this.userRepo = userRepo;
+	}
 	
 	@ModelAttribute("order")
 	public Order order() {
@@ -29,5 +54,32 @@ public class OrderController {
 	@GetMapping
 	public String showOrderForm() {
 		return "order";
+	}
+	
+	@PostMapping
+	public String processOrderForm(@Valid Order order, Errors errors, @ModelAttribute("cart") Cart cart, SessionStatus sessionStatus) {
+		
+		if(errors.hasErrors())
+			return "orderForm";
+		
+		List<OrderItem> orderItems = cart.getItems().stream()
+		                                            .map(item -> new OrderItem(item.getQuantity(), item.getProduct()))
+		                                            .collect(Collectors.toList());
+		
+		orderItems.forEach(orderItem -> order.addOrderItem(orderItem));
+		order.setTotal_price(cart.getTotalPrice());
+		
+	    Cart savedCart = cartRepo.save(cart);
+		
+		User user = order.getUser();
+		user.addOrder(order);
+		user.setCart(savedCart);
+		
+		userRepo.save(user);
+		
+		sessionStatus.isComplete();
+		
+		return "redirect:/home";
+		
 	}
 }
